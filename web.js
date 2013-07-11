ff2f8cdcb67b1a25be67f6f503d2eb17a6d63a67
erick8911 <erick_8911@hotmail.com>
2013-07-11 15:30:53 -0500

    read from index

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
+var fs = require('fs');
+var text = fs.readFileSync('index.html', 'utf8')
 
 app.get('/', function(request, response) {
-  response.send(fs.readFile("index.html"));
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
